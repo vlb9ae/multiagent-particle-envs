f61d0e19130ccc75db95b98d1f52371313fa1acb
vlb9ae <49457081+vlb9ae@users.noreply.github.com>
2021-05-02 21:22:36 -0400

    Update core.py

--- multiagent/core.py
+++ multiagent/core.py
@@ -144,6 +144,7 @@ class World(object):
         for i,agent in enumerate(self.agents):
             if agent.hopper:
                 p_force[i] = None
+                agent.state.p_pos += agent.action.u
             elif agent.movable:
                 noise = np.random.randn(*agent.action.u.shape) * agent.u_noise if agent.u_noise else 0.0
                 p_force[i] = agent.action.u + noise    
